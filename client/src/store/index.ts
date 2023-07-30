bc80cac33c5b423e0136e38ca3d03c6ed54b0af8
saiapowainur <saiapow.ainur@gmail.com>
2023-07-30 20:45:42 +0400

    fix: review and added error middleware to check correct auth on each request

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,19 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import usersReducer from 'src/store/slices/usersSlice'
-import userReducer from 'src/store/slices/userSlice'
-import competitionsReducer from 'src/store/slices/competitionsSlice'
-import competitionReducer from 'src/store/slices/competitionSlice'
+import { errorsMiddleware } from './middlewares/errorsMiddleware'
+import { rootReducer } from './rootReducer'
 
 export const store = configureStore({
-	reducer: {
-		users: usersReducer,
-		user: userReducer,
-		competitions: competitionsReducer,
-		competition: competitionReducer
-	}
+	reducer: rootReducer,
+	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(errorsMiddleware)
 })
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
