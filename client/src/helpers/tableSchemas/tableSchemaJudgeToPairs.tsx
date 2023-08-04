9cf0167e538d2c5ecbf6605ca4bc8fb7042aaf15
BulatSRC <bulatsrc@gmail.com>
2023-08-04 21:54:04 +0500

    merge updates

---
+++ client/src/helpers/tableSchemas/tableSchemaJudgeToPairs.tsx
@@ -0,0 +1,62 @@
+import { PairSchema, UserSchema } from 'src/types'
+import { getAge } from 'src/helpers/datetime'
+
+export type PairType = {
+    blackParticipantData?: UserSchema
+    whiteParticipantData?: UserSchema
+    judgeData?: UserSchema
+} & PairSchema
+
+export const tableSchemaJudgeToPairs = (tableData: PairSchema[], participants: UserSchema[], judges: UserSchema[]) => {
+    const participantsData = tableData.reduce((acc, pair) => {
+        const blackParticipantData = participants.find(({ _id }) => pair.blackParticipant === _id)
+        const whiteParticipantData = participants.find(({ _id }) => pair.whiteParticipant === _id)
+        // const judgeData = judges.find(({ _id }) => pair.judge === _id)
+
+        return [
+            ...acc,
+            {
+                ...pair,
+                blackParticipantData,
+                whiteParticipantData,
+                // judgeData
+            }
+        ]
+    }, [] as PairType[])
+
+    return participantsData.map((pair, i) => {
+        return {
+            cells: [
+                {
+                    node: <span>{i + 1}</span>,
+                    classes: 'max-w-[20px] text-base xl:font-bold xl:max-w-[50px]'
+                },
+                {
+                    node: (
+                        <div className='grid gap-[0.925rem] w-full lg:grid-cols-[2fr_1fr]'>
+                            <div className='flex justify-between'>
+                                <div className='w-[45%]'>
+                                    <p className='mb-[7px] text-sm text-black xl:text-base'>{pair.blackParticipantData?.fullName}</p>
+                                    <p className='text-xs xl:text-base'>
+                                        {getAge(pair.blackParticipantData?.birthDate as string)} age, {pair.blackParticipantData?.weight} kg
+                                    </p>
+                                </div>
+                                <span className='mx-[2%] w-[6%] text-sm text-black xl:text-base'>VS</span>
+                                <div className='w-[45%]'>
+                                    <div className='ml-auto w-fit'>
+                                        <p className='mb-[7px] text-sm text-black xl:text-base'>{pair.whiteParticipantData?.fullName}</p>
+                                        <p className=' text-xs xl:text-base'>
+                                            {getAge(pair.whiteParticipantData?.birthDate as string)} age, {pair.whiteParticipantData?.weight} kg
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>Select Judge</div>
+                        </div>
+                    ),
+                    classes: 'pl-0'
+                }
+            ]
+        }
+    })
+}
--- client/src/routes/JudgeAssignPage.tsx
+++ client/src/routes/JudgeAssignPage.tsx
@@ -1,12 +1,13 @@
-import { ReactElement, useEffect, useState } from 'react'
+import { ReactElement, useEffect, useState, Fragment } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { AppRoute } from 'src/constants/appRoute'
 import { ReactComponent as ArrowLeftIcon } from 'src/assets/arrow-left.svg'
 import { ReactComponent as WhatsAppIcon } from 'src/assets/whatsapp.svg'
 import { useAppDispatch, useAppSelector } from 'src/hooks/redux'
-import { Loader, Select } from 'src/ui'
+import { Loader, Select, TableBody, TableWrapper } from 'src/ui'
 import { getFormattedDate } from 'src/helpers/datetime'
-import { fetchCompetitionById } from 'src/store/slices/competitionSlice'
+import { fetchCompetitionById, fetchCompetitionJudges, fetchCompetitionParticipants } from 'src/store/slices/competitionSlice'
+import { tableSchemaJudgeToPairs } from 'src/helpers/tableSchemas/tableSchemaJudgeToPairs'
 
 export const JudgeAssignPage = (): ReactElement => {
     const { competitionId } = useParams()
@@ -14,12 +15,26 @@ export const JudgeAssignPage = (): ReactElement => {
     const dispatch = useAppDispatch()
     const competitionDataFromCompetitionsList = useAppSelector(s => s.competitions.data).find(({ _id }) => _id === competitionId)
     const competitionDataFromCompetition = useAppSelector(s => s.competition.data)
+    const judges = useAppSelector(s => s.competition.judges[competitionId as string])
     const competitionData = competitionDataFromCompetitionsList || competitionDataFromCompetition
+    const participants = useAppSelector(s => competitionId && s.competition.participants[competitionId])
     const dateStart = competitionData && getFormattedDate(competitionData.startDate, 'MMM D, HH:mm')
+
 
     useEffect(() => {
         if (!competitionData) {
             dispatch(fetchCompetitionById(competitionId as string))
+        }
+
+        if (!judges) {
+            dispatch(fetchCompetitionJudges(competitionId as string))
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    useEffect(() => {
+        if (!participants) {
+            dispatch(fetchCompetitionParticipants(competitionId as string))
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
@@ -33,43 +48,68 @@ export const JudgeAssignPage = (): ReactElement => {
             >
                 <ArrowLeftIcon/>
             </Link>
-            {competitionData
-                ? <div>
-                    <h1 className="mb-4 text-2xl font-semibold xl:text-[2rem] leading-normal">
-                        {competitionData.name}
-                    </h1>
-                    <p className="mb-[2.5rem] text-sm text-[#6C6A6C] xl:mb-[4.375rem] xl:text-xl xl:font-medium">
-                        {dateStart}
-                    </p>
-                </div>
-                : <Loader/>
-            }
-            <div>
-                <div className='mb-7 xl:mb-12'>
-                    <Select
-                        onChange={(value) => setSelectedGender(value)}
-                        menuOptions={[
-                            { value: 'Man', id: 'man' },
-                            { value: 'Woman', id: 'woman' }
-                        ]}
-                        dropdownPlaceholder='Select gender'
-                        placeholder='Select gender'
-                        chosenId={selectedGender}
-                    />
-                </div>
-                <div>
-                    <h3 className='mb-1 font-semibold xl:text-2xl'>Judges</h3>
-                    <div>
-                        <div>
-                            {/* <a className="flex-center gap-4 font-medium text-black text-sm hover:opacity-70 transition md:text-base xl:text-xl" */}
-                            {/*    href={`https://wa.me/${judge.socialNetworks?.whatsup}`}> */}
-                            {/*     <WhatsappIcon className="w-4 xl:w-6"/> */}
-                            {/*     {judge.socialNetworks?.whatsup} */}
-                            {/* </a> */}
+            <div className='lg:grid lg:grid-cols-2 lg:gap-7 xl:grid-cols-[auto_27rem]'>
+                {competitionData
+                    ? <div className='mb-4 xl:mb-[4.375rem]'>
+                        <h1 className="mb-4 text-2xl font-semibold xl:text-[2rem] leading-normal">
+                            {competitionData.name}
+                        </h1>
+                        <p className="text-sm text-[#6C6A6C]  xl:text-xl xl:font-medium">
+                            {dateStart}
+                        </p>
+                    </div>
+                    : <Loader/>
+                }
+                <div className='xl:mt-[-2.25rem]'>
+                    <div className='mb-6 xl:mb-12 xl:max-w-[23.375rem]'>
+                        <Select
+                            onChange={(value) => setSelectedGender(value)}
+                            menuOptions={[
+                                { value: 'Man', id: 'man' },
+                                { value: 'Woman', id: 'woman' }
+                            ]}
+                            dropdownPlaceholder='Select gender'
+                            placeholder='Select gender'
+                            chosenId={selectedGender}
+                        />
+                    </div>
+                    <div className='mb-5 md:mb-8 xl:mb-12 xl:ml-8'>
+                        <h3 className='mb-1 font-semibold lg:text-right lg:mb-3 xl:text-2xl'>Judges</h3>
+                        <div className='flex flex-wrap gap-3 lg:justify-between'>
+                            {judges && judges.map(judge => (
+                                <div className='flex flex-col' key={judge._id}>
+                                    <a className="flex items-center gap-4 font-medium text-black text-sm hover:opacity-70 transition md:text-base xl:text-xl"
+                                       href={`https://wa.me/${judge.socialNetworks?.whatsup}`}>
+                                        <WhatsAppIcon className="min-w-[0.875rem] w-3.5 lg:min-w-[1.5rem] lg:w-6"/>
+                                        {judge.fullName}
+                                    </a>
+                                    <span className='text-sm text-neutral-500 md:text-base'>{judge.fightClub?.name}</span>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
             </div>
+            {competitionData && (
+                <TableWrapper>
+                    {competitionData.groups?.map(({ _id, gender, ageCategory, weightCategory, passedPairs }) => (
+                        <Fragment key={_id}>
+                            <h3 className='mb-[17px] font-bold md:mb-[32px] xl:text-2xl [&:not(:first-child)]:border-t [&:not(:first-child)]:pt-[24px]'>
+                                <span className='capitalize'>{gender}</span> {ageCategory?.from}-{ageCategory?.to} age,{' '}
+                                {weightCategory?.from}-{weightCategory?.to}kg
+                                <span className='text-zinc-400'> {passedPairs?.length} pairs</span>
+                            </h3>
+                            {passedPairs && participants && judges ? (
+                                <TableBody rows={tableSchemaJudgeToPairs(passedPairs, participants, judges)} />
+                            ) : (
+                                <Loader />
+                            )}
+                        </Fragment>
+                    ))}
+                </TableWrapper>
+
+            )}
+
         </main>
     )
 }
