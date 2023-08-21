8ca85791ef40b394e8ca8b9895c04fcb0da4ad06
BulatSRC <bulatsrc@gmail.com>
2023-08-21 19:16:50 +0500

    draft

--- client/src/routes/JudgeAssignPage.tsx
+++ client/src/routes/JudgeAssignPage.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect, useState, useCallback, useRef } from 'react'
+import { ReactElement, useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { AppRoute } from 'src/constants/appRoute'
 import { ReactComponent as ArrowLeftIcon } from 'src/assets/arrow-left.svg'
@@ -28,7 +28,6 @@ export type SelectedJudge = {
 }
 
 export const JudgeAssignPage = (): ReactElement => {
-<<<<<<< HEAD
     const { competitionId } = useParams()
     const [selectedJudges, setSelectedJudges] = useState<SetJudgesToPairsSchema>()
     const dispatch = useAppDispatch()
@@ -44,25 +43,6 @@ export const JudgeAssignPage = (): ReactElement => {
     const judgeAssignSuccess = useAppSelector(s => s.competition.setPairJudgeSuccess)
     const submitError = useAppSelector(s => s.competition.setPairJudgeError)
     const [isAccordionOpen, setIsAccordionOpen] = useState<boolean | undefined>()
-=======
-	const { competitionId } = useParams()
-	const [selectedJudges, setSelectedJudges] = useState<SetJudgesToPairsSchema>()
-	const dispatch = useAppDispatch()
-	const navigate = useNavigate()
-	const competitionDataFromCompetitionsList = useAppSelector(s => s.competitions.data).find(
-		({ _id }) => _id === competitionId
-	)
-	const competitionDataFromCompetition = useAppSelector(s => s.competition.data)
-	const judges = useAppSelector(s => s.competition.judges[competitionId as string])
-	const competitionData = competitionDataFromCompetitionsList || competitionDataFromCompetition
-	const [groups, setGroups] = useState<CompetitionGroupSchema[] | undefined>()
-	const participants = useAppSelector(s => competitionId && s.competition.participants[competitionId])
-	const dateStart = competitionData && getFormattedDate(competitionData.startDate, 'MMM D, HH:mm')
-	const pending = useAppSelector(s => s.competition.judgeAssignPending)
-	const judgeAssignSuccess = useAppSelector(s => s.competition.setPairJudgeSuccess)
-	const submitError = useAppSelector(s => s.competition.setPairJudgeError)
-	const dragState = useRef({ dragIndex: 0, hoverIndex: 0 })
->>>>>>> bd3051dbab5fd1401b4f344502cb0ccafb5339a0
 
 	useEffect(() => {
 		if (!competitionData) {
@@ -112,10 +92,7 @@ export const JudgeAssignPage = (): ReactElement => {
 			navigate(`/${AppRoute.Competitions}/${competitionId}`)
 			dispatch(resetPairJudgeSuccessStatus())
 		}
-<<<<<<< HEAD
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-=======
->>>>>>> bd3051dbab5fd1401b4f344502cb0ccafb5339a0
 	}, [judgeAssignSuccess])
 
 	const handleJudgeSelect = (updatedJudgeData: SelectedJudge) => {
@@ -131,7 +108,6 @@ export const JudgeAssignPage = (): ReactElement => {
 			judgesByGroups: newSelectJudge
 		})
 	}
-<<<<<<< HEAD
 
     const handleDoneClick = () => {
         dispatch(setPairJudges(selectedJudges as SetJudgesToPairsSchema))
@@ -227,116 +203,4 @@ export const JudgeAssignPage = (): ReactElement => {
             </BottomFixedContainer>
         </main>
     )
-=======
-
-	const handleDoneClick = () => {
-		dispatch(setPairJudges(selectedJudges as SetJudgesToPairsSchema))
-	}
-
-	const moveItems = useCallback((dragIndex: number, hoverIndex: number) => {
-		if (dragState.current.dragIndex !== dragIndex || dragState.current.hoverIndex !== hoverIndex) {
-			setGroups(prevGroups => swapArray(prevGroups as CompetitionGroupSchema[], dragIndex, hoverIndex))
-			dragState.current = { dragIndex, hoverIndex }
-		}
-	}, [])
-
-	return (
-		<main className='container mx-auto grow px-[17px] pt-8 pb-[5.5rem] md:py-9 md:pb-28 xl:pt-14 xl:pl-[7.5rem] xl:pr-[7.5rem]'>
-			<div className='mb-8 xl:mb-12 xl:flex xl:justify-between xl:gap-6'>
-				{competitionData ? (
-					<div className='mb-6 xl:mb-0 xl:max-w-[34.375rem]'>
-						<h1 className='mb-1.5 text-lg font-medium xl:text-xl'>{competitionData.name}</h1>
-						<p className='mb-6 text-[#6C6A6C] xl:mb-11'>{dateStart}</p>
-						<div className='relative mb-1.5'>
-							<h2 className='text-2xl font-semibold xl:text-4xl xl:font-bold'>
-								Connect judges to pairs and assign orders to groups 3/3
-							</h2>
-							<button
-								className='hidden transition hover:opacity-70 xl:absolute xl:left-[-3.5rem] xl:top-0 xl:block'
-								onClick={() => navigate(-1)}
-								type='button'
-							>
-								<ArrowLeftIcon />
-							</button>
-						</div>
-					</div>
-				) : (
-					<Loader />
-				)}
-				<div className='mb-5 md:mb-8 xl:mb-12 xl:ml-8'>
-					<h3 className='mb-1 font-semibold lg:mb-3 xl:text-right xl:text-2xl'>Judges</h3>
-					<div className='flex flex-wrap gap-3 lg:gap-6 xl:justify-between'>
-						{judges &&
-							judges.map(judge => (
-								<div className='flex flex-col' key={judge._id}>
-									<a
-										className='flex items-center gap-4 text-sm font-medium text-black transition hover:opacity-70 md:text-base xl:text-xl'
-										href={`https://wa.me/${judge.socialNetworks?.whatsup}`}
-									>
-										<WhatsAppIcon className='w-3.5 min-w-[0.875rem] lg:w-6 lg:min-w-[1.5rem]' />
-										{judge.fullName}
-									</a>
-									<span className='text-sm text-neutral-500 md:text-base'>{judge.fightClub?.name}</span>
-								</div>
-							))}
-					</div>
-				</div>
-			</div>
-			{groups && (
-				<TableWrapper classes='py-4 xl:py-7'>
-					<DndProvider backend={HTML5Backend}>
-						{groups.map(({ _id: groupId, gender, ageCategory, weightCategory, currentRoundPairs }, i) => (
-							<Accordion
-								moveItem={moveItems}
-								id={groupId as string}
-								index={i}
-								key={groupId}
-								classes='last:pb-0'
-								isDraggable
-								title={
-									<h3 className='font-bold xl:text-2xl [&:not(:first-child)]:border-t [&:not(:first-child)]:pt-[24px]'>
-										<span className='capitalize'>{gender}</span> {ageCategory?.from}-{ageCategory?.to} age,{' '}
-										{weightCategory?.from}-{weightCategory?.to}kg
-										{currentRoundPairs?.length && (
-											<span className='text-zinc-400'>
-												{' '}
-												{currentRoundPairs?.length} {`pair${currentRoundPairs?.length === 1 ? '' : 's'}`}
-											</span>
-										)}
-									</h3>
-								}
-							>
-								{currentRoundPairs && participants && judges ? (
-									<TableBody
-										rows={tableSchemaJudgeToPairs({
-											tableData: currentRoundPairs,
-											participants,
-											judges,
-											groupId,
-											selectedJudges: selectedJudges?.judgesByGroups && selectedJudges?.judgesByGroups[i],
-											onSelect: handleJudgeSelect
-										})}
-									/>
-								) : (
-									<Loader />
-								)}
-							</Accordion>
-						))}
-					</DndProvider>
-				</TableWrapper>
-			)}
-			<BottomFixedContainer classes='xl:pl-[7.5rem] xl:pr-[7.5rem]'>
-				<div className='flex flex-wrap gap-2.5'>
-					<Button type='outlined' onClick={() => navigate(`../${AppRoute.CreateGroup}`)}>
-						Previous step
-					</Button>
-					<Button classes='min-w-[8rem] xl:min-w-[15.625rem]' onClick={handleDoneClick} loading={pending}>
-						Done
-					</Button>
-					{submitError && <Alert type='error' subtitle={submitError} />}
-				</div>
-			</BottomFixedContainer>
-		</main>
-	)
->>>>>>> bd3051dbab5fd1401b4f344502cb0ccafb5339a0
 }
