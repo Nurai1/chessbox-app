0134893bf0b707efec4d038a3d7228c49764b925
unknown <bulatsrc@gmail.com>
2023-10-10 12:21:19 +0500

    fix conflicts

--- client/src/components/PrivateRoute/PrivateRoute.tsx
+++ client/src/components/PrivateRoute/PrivateRoute.tsx
@@ -2,16 +2,23 @@ import { FC, ReactElement } from 'react'
 import { Navigate } from 'react-router-dom'
 import { useAppSelector } from 'src/hooks/redux'
 import { AuthorizationStatus } from 'src/constants/authorizationStatus'
+import { Role } from 'src/constants/role'
 import { AppRoute } from 'src/constants/appRoute'
 import { Loader } from 'src/ui'
 
 type PrivateRoutePropsType = {
 	children: ReactElement
+	role: Role.Admin | Role.Judge | Role.ChiefJudge | Role.Participant
 	redirectPath?: string
 }
 
-const PrivateRoute: FC<PrivateRoutePropsType> = ({ children, redirectPath = `/${AppRoute.SignIn}` }): ReactElement => {
+const PrivateRoute: FC<PrivateRoutePropsType> = ({
+	children,
+	redirectPath = `/${AppRoute.NotFound}`,
+	role
+}): ReactElement => {
 	const authorizationStatus = useAppSelector(state => state.user.authorizationStatus)
+	const authorizedUserRole = useAppSelector(state => state.user.authorizedUser)?.role
 
 	if (authorizationStatus === AuthorizationStatus.Unknown) {
 		return <Loader />
@@ -21,7 +28,15 @@ const PrivateRoute: FC<PrivateRoutePropsType> = ({ children, redirectPath = `/${
 		return <Navigate to={redirectPath} />
 	}
 
-	return children
+	if (authorizedUserRole === role) {
+		return children
+	}
+
+	if (AuthorizationStatus.Auth) {
+		return children
+	}
+
+	return <Navigate to={redirectPath} />
 }
 
 export { PrivateRoute }
