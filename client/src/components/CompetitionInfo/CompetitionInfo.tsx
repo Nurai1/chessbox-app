0b3777230c471c55542279ba65c49dbb1e718a81
unknown <bulatsrc@gmail.com>
2023-11-08 19:50:43 +0500

    fix conflicts and adds participants place to competitions list

--- client/src/components/CompetitionInfo/CompetitionInfo.tsx
+++ client/src/components/CompetitionInfo/CompetitionInfo.tsx
@@ -12,18 +12,27 @@ type CompetitionInfoPropsType = {
 export const CompetitionInfo: FC<CompetitionInfoPropsType> = ({ title, img, place, isCompetitionPage, classes }) => {
 	return (
 		<div
-			className={twMerge(`${
-				isCompetitionPage
-					? 'lg:h-fit lg:min-h-[9rem] lg:items-baseline lg:p-4 xl:min-h-[14rem] xl:items-baseline xl:justify-start xl:p-9'
-					: 'lg:min-h-[9.375rem] lg:p-6 xl:h-fit'
-			} flex items-center justify-center gap-3 p-3 lg:border lg:rounded-3xl lg:relative lg:items-baseline lg:justify-start`, classes)}
+			className={twMerge(
+				`${
+					isCompetitionPage
+						? 'lg:h-fit lg:min-h-[9rem] lg:items-baseline lg:p-4 xl:min-h-[14rem] xl:items-baseline xl:justify-start xl:p-9'
+						: 'lg:min-h-[9.375rem] lg:p-6 xl:h-fit'
+				} flex items-center justify-center gap-3 p-3 lg:relative lg:items-baseline lg:justify-start lg:rounded-3xl lg:border`,
+				classes
+			)}
 		>
 			<div>
-				<h3 className={`${isCompetitionPage? 'xl:text-heading-3': 'lg:text-heading-4'} font-bold`}>{title}</h3>
+				<h3 className={`${isCompetitionPage ? 'xl:text-heading-3' : 'lg:text-heading-4'} font-bold`}>{title}</h3>
 				{place && (
-					<div className='mt-2 flex lg:flex-col-reverse xl:mt-3'>
-						<p className='text-sm text-grey xl:-mt-2.5'>Your place&nbsp;</p>
-						<p className='text-sm text-grey lg:text-2xl lg:font-bold lg:text-black xl:text-heading-1'>{place}</p>
+					<div className={`${isCompetitionPage ? 'lg:flex-col-reverse xl:mt-3' : ''} mt-2 flex`}>
+						<p className={`${isCompetitionPage ? 'xl:-mt-2.5' : ''} text-sm text-grey`}>Your place&nbsp;</p>
+						<p
+							className={`${
+								isCompetitionPage ? 'lg:text-2xl lg:font-bold lg:text-black xl:text-heading-1' : ''
+							} text-sm text-grey `}
+						>
+							{place}
+						</p>
 					</div>
 				)}
 			</div>
