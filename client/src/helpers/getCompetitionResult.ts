cb9187eadf71b1fdb6c852b5de9f652d7cfddded
saiapowainur <saiapow.ainur@gmail.com>
2023-11-08 21:39:19 +0800

    small fixes

--- client/src/helpers/getCompetitionResult.ts
+++ client/src/helpers/getCompetitionResult.ts
@@ -2,26 +2,26 @@ import { CompetitionSchema, CompetitionGroupSchema } from 'src/types'
 import { Gender } from 'src/constants/gender'
 
 type CompetitionResultType = {
-    [Gender.Woman]: CompetitionGroupSchema[]
-    [Gender.Man]: CompetitionGroupSchema[]
+	[Gender.Woman]: CompetitionGroupSchema[]
+	[Gender.Man]: CompetitionGroupSchema[]
 }
 
 export const getCompetitionResult = (competitionData: CompetitionSchema) => {
-    if (competitionData && competitionData.groups && competitionData.groups[0].results?.length !== 0) {
-            return competitionData.groups?.reduce(
-                (acc, group) => {
-                    if (group.gender === Gender.Woman) {
-                        acc[Gender.Woman].push(group)
-                    } else {
-                        acc[Gender.Man].push(group)
-                    }
-                    return acc
-                },
-                {
-                    [Gender.Woman]: [],
-                    [Gender.Man]: []
-                } as CompetitionResultType
-            )
-        }
-    return null
+	if (competitionData && competitionData.groups && competitionData.groups[0]?.results?.length !== 0) {
+		return competitionData.groups?.reduce(
+			(acc, group) => {
+				if (group.gender === Gender.Woman) {
+					acc[Gender.Woman].push(group)
+				} else {
+					acc[Gender.Man].push(group)
+				}
+				return acc
+			},
+			{
+				[Gender.Woman]: [],
+				[Gender.Man]: []
+			} as CompetitionResultType
+		)
+	}
+	return null
 }
