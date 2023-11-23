271433b76d33e7675d1a35a63d5dd9b8d1ae7789
saiapowainur <saiapow.ainur@gmail.com>
2023-11-23 19:36:09 +0800

    fix: requirements

--- client/src/helpers/checkParticipantFitRequirements.ts
+++ client/src/helpers/checkParticipantFitRequirements.ts
@@ -2,13 +2,13 @@ import { CompetitionSchema, UserSchema } from 'src/types'
 import { getAge } from './datetime'
 
 export const checkParticipantFitRequirements = (competitionData?: CompetitionSchema, authorizedUser?: UserSchema) => {
-	if (authorizedUser?.weight && authorizedUser?.birthDate) {
+	if (authorizedUser?.weight && authorizedUser?.birthDate && competitionData?.requirements) {
 		return (
-			authorizedUser.weight < (competitionData?.requirements?.weightCategory?.from ?? 199) ||
-			authorizedUser.weight > (competitionData?.requirements?.weightCategory?.to ?? 0) ||
-			(getAge(authorizedUser.birthDate) as number) < (competitionData?.requirements?.ageCategory?.from ?? 120) ||
-			(getAge(authorizedUser.birthDate) as number) > (competitionData?.requirements?.ageCategory?.to ?? 0) ||
-			(competitionData?.requirements?.gender && authorizedUser.gender !== competitionData.requirements.gender)
+			authorizedUser.weight < (competitionData.requirements?.weightCategory?.from ?? 0) ||
+			authorizedUser.weight > (competitionData.requirements?.weightCategory?.to ?? 199) ||
+			(getAge(authorizedUser.birthDate) as number) < (competitionData.requirements?.ageCategory?.from ?? 0) ||
+			(getAge(authorizedUser.birthDate) as number) > (competitionData.requirements?.ageCategory?.to ?? 119) ||
+			(competitionData.requirements?.gender && authorizedUser.gender !== competitionData.requirements.gender)
 		)
 	}
 	return false
