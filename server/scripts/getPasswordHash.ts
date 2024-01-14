999d2d777cbf59d4e48e432524ea8313337d4e8f
saiapowainur <saiapow.ainur@gmail.com>
2024-01-14 14:19:12 +0600

    feat: add script for password hash

---
+++ server/scripts/getPasswordHash.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+import bcrypt from 'bcryptjs';
+
+const Readline = require('node:readline');
+
+const readline = Readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function getPasswordHash(password: string) {
+  return bcrypt.hash(password, 10);
+}
+
+readline.question(`What's the password?\n`, (password: string) => {
+  console.log(`Password: ${password}`);
+  readline.close();
+
+  getPasswordHash('Qwerty12').then((hash) => {
+    console.log(`Password hash: ${hash}`);
+  });
+});
