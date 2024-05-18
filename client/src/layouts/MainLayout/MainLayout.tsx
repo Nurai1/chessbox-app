769811ba544631a80e19efea6c741d84d3a784a5
saiapowainur <saiapow.ainur@gmail.com>
2024-05-19 02:40:57 +0500

    feat: added i18n info modal

--- client/src/layouts/MainLayout/MainLayout.tsx
+++ client/src/layouts/MainLayout/MainLayout.tsx
@@ -1,12 +1,25 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Header } from 'src/components'
+import { Modal } from '../../ui'
 
 export const MainLayout = (): ReactElement => {
+	const [showI18nWarningModal, setShowI18nWarningModal] = useState(!localStorage.getItem('showedI18nWarningModal'))
+
 	return (
 		<div className='ios_scroll_issue relative flex h-screen min-h-screen flex-col overflow-x-hidden'>
 			<Header />
 			<Outlet />
+			<Modal
+				isOpen={showI18nWarningModal}
+				onClose={() => {
+					setShowI18nWarningModal(false)
+					localStorage.setItem('showedI18nWarningModal', 'true')
+				}}
+				modalType='regular'
+				content={<div className='flex-center'>Вы можете менять язык справа сверху в приложении.</div>}
+				title='Русский язык появился!'
+			/>
 		</div>
 	)
 }
